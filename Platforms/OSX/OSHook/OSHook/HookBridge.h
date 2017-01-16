bd5988d62df91246fc9d4047473e52b1f8ab8861
Setsuodu <339131001@qq.com>
2017-01-16 11:37:03 +0800

    platform folder
    platform source code

---
+++ Platforms/OSX/OSHook/OSHook/HookBridge.h
@@ -0,0 +1,17 @@
+//
+//  HookBridge.h
+//  OSHook
+//
+//  Created by 薛宇涛 on 2017/1/15.
+//  Copyright © 2017年 薛宇涛. All rights reserved.
+//
+
+extern "C"{
+    
+    void CallMethod();
+    
+    const char* ReturnString();
+    
+    int ReturnInt();
+    
+}
---
+++ Platforms/iOS/OSHook/OSHook/HookBridge.cpp
@@ -0,0 +1,24 @@
+//
+//  HookBridge.cpp
+//  OSHook
+//
+//  Created by 薛宇涛 on 2017/1/15.
+//  Copyright © 2017年 薛宇涛. All rights reserved.
+//
+
+#include "HookBridge.h"
+
+
+void CallMethod(){
+    // To do some things.
+}
+
+
+const char* ReturnString(){
+    return "hello there";
+}
+
+
+int ReturnInt(){
+    return 5;
+}
